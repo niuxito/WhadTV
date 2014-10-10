451c725b13f826593a53997887685d3d28138837
Pedro <morenox24@gmail.com>
2014-10-10 18:01:08 +0200

    Modelos Conector y ConectorSecret.

---
+++ app/Model/ConectorSecret.php
@@ -0,0 +1,20 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * ConectorSecret Model
+ *
+ */
+class ConectorSecret extends AppModel {
+/**
+ * Use table
+ *
+ * @var mixed False or table name
+ */
+	public $useTable = 'conectorSecret';
+/**
+ * Primary key field
+ *
+ * @var string
+ */
+	public $primaryKey = 'id';
+}
